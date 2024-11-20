b7afda2983fe0f1fd53de58e16dbb284b16fc6c3
Darwin Ameli <DIDI.ameli@gmail.com>
2024-11-20 22:08:45 +0100

    Add files via upload

---
+++ Models/WeatherWidget.cs
@@ -0,0 +1,41 @@
+﻿namespace maui_meteosuisse.Models;
+
+public class WeatherWidget
+{
+    public Data data { get; set; }
+    public Config config { get; set; }
+}
+
+public class Data
+{
+    public int altitude { get; set; }
+    public string city_name { get; set; }
+    public Current current { get; set; }
+    public int weather_symbol_id { get; set; }
+    public string location_id { get; set; }
+    public Forecast[] forecasts { get; set; }
+    public int timestamp { get; set; }
+}
+
+public class Current
+{
+    public string temperature { get; set; }
+    public string weather_symbol_id { get; set; }
+}
+
+public class Forecast
+{
+    public long noon { get; set; }
+    public string temp_high { get; set; }
+    public string weekday { get; set; }
+    public string temp_low { get; set; }
+    public string weather_symbol_id { get; set; }
+}
+
+public class Config
+{
+    public string name { get; set; }
+    public string language { get; set; }
+    public string version { get; set; }
+    public int timestamp { get; set; }
+}
